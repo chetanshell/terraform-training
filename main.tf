8a840dd18eb704bede3189dd3fe755f282b66667
chetanshell <37759377+chetanshell@users.noreply.github.com>
2019-01-31 20:43:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,7 @@ resource "aws_instance" {
   ami           = "${var.ami}"
   instance_type = "${var.instance_type}"
 
-  tags = "${merge(var.default_tags, map("Name","${var.project}-${var.environment}-${element(var.azs,count.index)}-wowza-0${count.index + 1}"))}"
-
+ 
   key_name               = "${var.key_name}"
   vpc_security_group_ids = "${var.security_group_ids}"
   subnet_id              = "${element(var.subnets, count.index)}"
