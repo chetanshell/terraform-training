3f356f65f5cfbb5f92753383fd023c7fdc9b60bd
chetanshell <37759377+chetanshell@users.noreply.github.com>
2019-01-31 21:41:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   profile = "${var.profile}"
 }
 
-resource "aws_instance" {
+resource "aws_instance" "example" {
   ami           = "${var.ami}"
   instance_type = "${var.instance_type}"
 
